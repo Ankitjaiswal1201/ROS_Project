b3380ad21fc74e1e2480289f7b76ac15ff326fe6
Ankit Jaiswal <ankit.jaiswal@s2017.tu-chemnitz.de>
2019-07-12 09:35:52 +0200

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ankit/ros_project_dependencies_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/ankit/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/ankit/catkin_ws/devel;/opt/ros/melodic" != "" else []
